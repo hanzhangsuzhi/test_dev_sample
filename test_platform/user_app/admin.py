892290a4932e2ccce47072a80ceb8bdab1d60def
guqiong <346038425@qq.com>
2018-09-21 23:33:08 +0800

    submit code

---
+++ test_platform/user_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+# Register your models here.
+# django自带了1个admin后台，把模型中的表映射到后台，由后台来管理
---
+++ test_platform/user_app/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+# ORM创建数据库表
+
